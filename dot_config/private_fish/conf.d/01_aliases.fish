4ccccf1c239f8c0dee89cbf7b0febc0909c8b356
devrtc0 <herr.mazik@gmail.com>
2021-05-30 13:47:08 +0400

    smol changes

--- dot_config/private_fish/conf.d/01_aliases.fish
+++ dot_config/private_fish/conf.d/01_aliases.fish
@@ -1,8 +1,9 @@
-alias ls="exa -lag"
 alias df="df -h"
 alias du="du -hs"
-alias ca="cargo"
-alias v="nvim"
+
+if type cargo >/dev/null
+    alias ca="cargo"
+end
 
 abbr -a gw "./gradlew"
 abbr -a vmv "sudo modprobe -a vmw_vmci vmmon && sudo systemctl start vmware-networks.service vmware-usbarbitrator.service"
